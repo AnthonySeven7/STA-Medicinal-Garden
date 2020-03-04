8e4d6eec2c48b14d159706524d676483e9f64a08
Anthony Neves <anthony.neves77@gmail.com>
2020-03-04 17:48:53 +0000

    Resolved canvas resize issue. Began working on UI animations and general appearence.

---
+++ Assets/Scripts/UIManger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class UIManger : MonoBehaviour
+{
+    public RectTransform generalInfo, background;
+    private float genInfo_y;
+    // Start is called before the first frame update
+    void Start()
+    {
+        genInfo_y = generalInfo.transform.position.y;
+        Debug.Log(genInfo_y);
+    }
+
+    public void GeneralInfoButton()
+    {
+        var x = 0.0f;
+        if (generalInfo.transform.position.y == 530.0f)
+        {
+            x = genInfo_y;
+        }
+        generalInfo.DOAnchorPos(new Vector2(0, x), 0.25f).SetDelay(0.25f);
+        background.DOAnchorPos(new Vector2(0, x), 0.25f).SetDelay(0.25f);
+    }
+}
